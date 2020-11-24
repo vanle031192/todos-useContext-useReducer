e4bd264cfb3000cbb2a8387d20b5e87d798dd201
vanle031192 <hvbk3933@gmail.com>
2020-11-24 17:33:34 +0700

    Updates

---
+++ precache-manifest.fdf98378e202883312206df7b62de53d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5fd5e5c707bf357e842",
+    "url": "/todos-useContext-useReducer/static/css/main.4ad28f95.chunk.css"
+  },
+  {
+    "revision": "d5fd5e5c707bf357e842",
+    "url": "/todos-useContext-useReducer/static/js/main.8e2d0e78.chunk.js"
+  },
+  {
+    "revision": "b75bc5f9f1aa1f5907e9",
+    "url": "/todos-useContext-useReducer/static/js/runtime~main.617cc9d4.js"
+  },
+  {
+    "revision": "dbbd8b11ce29453dad76",
+    "url": "/todos-useContext-useReducer/static/js/2.950f7ec8.chunk.js"
+  },
+  {
+    "revision": "69dea806e0dbc0ec94f77a045c8c680d",
+    "url": "/todos-useContext-useReducer/index.html"
+  }
+];
